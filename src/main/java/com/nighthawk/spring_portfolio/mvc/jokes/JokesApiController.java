3ade9bdb424d6d5f56cc54a1097ce81fce8e64b9
BobTheFarmer <colinmweis@gmail.com>
2023-10-30 09:03:52 -0700

    Copied + Phrases

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/JokesApiController.java
@@ -0,0 +1,35 @@
+package com.nighthawk.spring_portfolio.mvc.jokes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController // annotation to simplify the creation of RESTful web services
+@RequestMapping("/api/phrases")  // all requests in file begin with this URI
+public class JokesApiController {
+
+    // Autowired enables Control to connect URI request and POJO Object to easily for Database CRUD operations
+    @Autowired
+    private JokesJpaRepository repository;
+
+    /* GET List of Jokes
+     * @GetMapping annotation is used for mapping HTTP GET requests onto specific handler methods.
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<Jokes>> getJokes() {
+        // ResponseEntity returns List of Jokes provide by JPA findAll()
+        return new ResponseEntity<>( repository.findAll(), HttpStatus.OK);
+    }
+
+    /* Update Like
+     * @PutMapping annotation is used for mapping HTTP PUT requests onto specific handler methods.
+     * @PathVariable annotation extracts the templated part {id}, from the URI
+     */
+
+    /* Update Jeer
+     */
+}
