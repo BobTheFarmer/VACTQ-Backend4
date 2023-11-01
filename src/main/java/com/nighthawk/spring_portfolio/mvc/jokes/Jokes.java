21ac1df07c8b26ba7fcdf7f1d1ef5c9d66a0323d
VINERAJ <vinay.e.rajagopalan@gmail.com>
2023-11-01 11:43:54 -0700

    inputted actual phrases

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/Jokes.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/Jokes.java
@@ -23,24 +23,25 @@ public class Jokes {
     // starting jokes
     public static String[] init() {
         final String[] jokesArray = {
-            "If you give someone a program... you will frustrate them for a day; if you teach them how to program... you will frustrate them for a lifetime.",
-            "Q: Why did I divide sin by tan? A: Just cos.",
-            "UNIX is basically a simple operating system... but you have to be a genius to understand the simplicity.",
-            "Enter any 11-digit prime number to continue.",
-            "If at first you don't succeed; call it version 1.0.",
-            "Java programmers are some of the most materialistic people I know, very object-oriented",
-            "The oldest computer can be traced back to Adam and Eve. It was an apple but with extremely limited memory. Just 1 byte. And then everything crashed.",
-            "Q: Why did Wi-Fi and the computer get married? A: Because they had a connection",
-            "Bill Gates teaches a kindergarten class to count to ten. 1, 2, 3, 3.1, 95, 98, ME, 2000, XP, Vista, 7, 8, 10.",
-            "Q: What’s a aliens favorite computer key? A: the space bar!",
-            "There are 10 types of people in the world: those who understand binary, and those who don’t.",
-            "If it wasn't for C, we’d all be programming in BASI and OBOL.",
-            "Computers make very fast, very accurate mistakes.",
-            "Q: Why is it that programmers always confuse Halloween with Christmas? A: Because 31 OCT = 25 DEC.",
-            "Q: How many programmers does it take to change a light bulb? A: None. It’s a hardware problem.",
-            "The programmer got stuck in the shower because the instructions on the shampoo bottle said: Lather, Rinse, Repeat.",
-            "Q: What is the biggest lie in the entire universe? A: I have read and agree to the Terms and Conditions.",
-            "An SQL statement walks into a bar and sees two tables. It approaches, and asks may I join you?"
+            "Java is an object-oriented programming language that is concurrent, class-based, and specifically designed to have as few implementation dependencies as possible.",
+            "In Java, every piece of data, whether it be a number, a character, or an object, is created in the memory.",
+            "One of the key features of Java is garbage collection; it automatically deletes objects that are not being used.",
+            "Java uses Just-In-Time compiler for high performance; JIT compiles parts of the bytecode that have similar functionality at the same time, and hence reduces the amount of time needed for compilation.",
+            "In Java, classes and objects from its methods are the fundamental units of OOPs which make it a powerful language.",
+            "Java provides a robust system with its strong exception handling and type checking mechanism which makes it a secure language.",
+            "Multithreading in Java is a feature that allows concurrent execution of two or more parts of a program for maximum utilization of CPU.",
+            "Java supports automatic garbage collection; it tracks each and every object available in the JVM heap space and removes unused ones.",
+            "In Java, there is no need to remove unreferenced objects because there is an Automatic Garbage Collection in java.",
+            "Java provides APIs for networking, database connection, utilities and almost everything; whatever left can be accomplished by third-party libraries.",
+            "Java is platform-independent because when you compile a Java program, it gets converted to bytecode which can be interpreted on any system.",
+            "The concept of exception handling in Java gives us a way to handle the runtime errors so that normal flow of the application can be maintained.",
+            "Inheritance in Java is a mechanism where one object acquires all the properties and behaviors of the parent object.",
+            "Java's 'write once, run anywhere' approach means it has significant advantages over other languages for certain types of applications.",
+            "Java's standard data types are known as primitive types; they include `int`, `short`, `long`, `byte`, `float`, `double`, `char`, and `boolean`.",
+            "A class in Java is a blueprint which includes all your data and methods to be worked upon.",
+            "Java's encapsulation feature allows wrapping code and data together into a single unit, like a capsule which is mixed with several medicines.",
+            "An interface in Java is a blueprint of a class or you can say it is a collection of abstract methods and static constants.",
+            "In Java, method overloading is not considered as polymorphism because overload resolution takes place at compile time."
         };
         return jokesArray;
     }
