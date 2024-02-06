99cb35e6905ff1603723e618c79f58439c8e8643
aaron-rub <aaronr06138@gmail.com>
2024-02-05 20:48:39 -0800

    Income Testing

--- src/main/java/com/nighthawk/spring_portfolio/mvc/player/Player.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/player/Player.java
@@ -1,3 +1,4 @@
+package com.nighthawk.spring_portfolio.mvc.player;
 import javax.persistence.*;
 import java.util.List;
 
@@ -16,14 +17,14 @@ public class Player {
     private String uid;
 
     @Column(name = "distance", nullable = false)
-    private String distance;
+    private int distance;
 
     // getters and setters
 
-    public Player(String name, String uid, String distance) {
+    public Player(String name, String uid, int i) {
         this.name = name;
         this.uid = uid;
-        this.distance = distance;
+        this.distance = i;
     }
 // getters and setters
 
@@ -31,5 +32,26 @@ Player player1 = new Player("Azeem Khan", "azeemK", 45);
 Player player2 = new Player("Ahad Biabani", "ahadB", 41);
 Player player3 = new Player("Akshat Parikh", "akshatP", 40);
 Player player4 = new Player("Josh Williams", "joshW", 38);
+
+public String getUid() {
+    return null;
+}
+
+public Object getName() {
+    return null;
+}
+
+public void setName(Object name2) {
+}
+
+public void setUid(String uid2) {
+}
+
+public Object getDistance() {
+    return null;
+}
+
+public void setDistance(Object distance2) {
+}
 
 }
